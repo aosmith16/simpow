5a9d709ab0eb081520adc9a42895812605b8a7ed
Ariel Muldoon <ariel.muldoon@oregonstate.edu>
2021-06-29 13:12:18 -0700

    Add test for vary_size and more to lmm_f

--- tests/testthat/test-lmm_f.R
+++ tests/testthat/test-lmm_f.R
@@ -35,3 +35,36 @@ test_that("data length matches design parameters", {
                 keep_data = TRUE)
     expect_equal(nrow(res$data[[1]]), ntrt*nblock*nrep)
 })
+
+test_that("data is ordered by blocks then treatments", {
+
+    res1 = lmm_f(test = "none",
+                ntrt = 2,
+                trtmeans = c(1, 25),
+                nblock = 3,
+                nrep = 1,
+                sd_block = 2,
+                sd_resid = 4,
+                keep_data = TRUE)$data[[1]]
+
+    trt1 = rep(LETTERS[1:2], times = 3)
+    block1 = rep(as.character(1:3), each = 2)
+
+    expect_equal(res1[, 1], trt1)
+    expect_equal(res1[, 2], block1)
+
+    res2 = lmm_f(test = "none",
+                 ntrt = 2,
+                 trtmeans = c(1, 25),
+                 nblock = 3,
+                 nrep = 3,
+                 sd_block = 2,
+                 sd_resid = 4,
+                 keep_data = TRUE)$data[[1]]
+
+    trt2 = rep(LETTERS[1:2], times = 3*3)
+    block2 = rep(as.character(1:3), each = 2*3)
+
+    expect_equal(res2[, 1], trt2)
+    expect_equal(res2[, 2], block2)
+})
---
+++ tests/testthat/test-vary_size.R
@@ -0,0 +1,24 @@
+test_that("returned output is same manual calls", {
+    set.seed(16)
+    res = vary_size(tovary = "nblock",
+                    values = c(5, 15),
+                    nsim = 10,
+                    trtmeans = c(1, 2),
+                    sd_block = 2,
+                    sd_resid = 2)
+
+    set.seed(16)
+    man1 = summary(lmm_f(nsim = 10,
+                         trtmeans = c(1, 2),
+                         nblock = 5,
+                         sd_block = 2,
+                         sd_resid = 2))
+
+    man2 = summary(lmm_f(nsim = 10,
+                         trtmeans = c(1, 2),
+                         nblock = 15,
+                         sd_block = 2,
+                         sd_resid = 2))
+
+    expect_equal(res, rbind(man1, man2))
+})
